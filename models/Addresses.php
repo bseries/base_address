851375c6d54adf40687c82bdde0ff22c9f6ef235
David Persson <davidpersson@gmx.de>
2015-03-02 12:57:49 +0100

    WIP

--- models/Addresses.php
+++ models/Addresses.php
@@ -13,9 +13,14 @@
 namespace base_address\models;
 
 use lithium\core\Environment;
-use base_address\models\Countries;
 use lithium\util\Validator;
+use lithium\util\Inflector;
 use lithium\g11n\Message;
+use CommerceGuys\Addressing\Model\Address as FormalAddress;
+use CommerceGuys\Addressing\Formatter\PostalFormatter;
+use CommerceGuys\Addressing\Provider\DataProvider;
+use CommerceGuys\Addressing\Validator\Constraints\AddressFormat;
+use CommerceGuys\Addressing\Validator\Constraints\Country;
 
 class Addresses extends \base_core\models\Base {
 
@@ -37,21 +42,124 @@ class Addresses extends \base_core\models\Base {
 	];
 
 
+	public static function _validateAddress($address) {
+		$validates = [];
+
+//		$countryCode = $address->getCountryCode();
+
+		$fields = [
+			'administrativeArea',
+			'locality',
+			'dependentLocality',
+			'postalCode',
+			'sortingCode',
+			'addressLine1',
+			// skip address line 2
+			'organization',
+			'recipient'
+		];
+
+		$values = [];
+        foreach ($this->fieldMapping as $field) {
+            $getter = 'get' . ucfirst($field);
+            $values[Inflector::underscore($field)] = $address->$getter();
+        }
+
+		$dataProvider = new DataProvider();
+		$addressFormat = $dataProvider->getAddressFormat($address->getCountryCode());
+
+		// Require fields
+
+	}
+
+	protected static function _validateSubdivision($field, $dataProvider, $addressFormat) {
+		$parentId = ($fieldConstant == 'root') ? 0 : $values[$fieldConstant];
+		$children = $dataProvider->getSubdivisions($countryCode, $parentId);
+
+		$levels = array(
+            'root',
+            AddressFormatInterface::FIELD_ADMINISTRATIVE_AREA,
+            AddressFormatInterface::FIELD_LOCALITY,
+            AddressFormatInterface::FIELD_DEPENDENT_LOCALITY,
+        );
+
+	}
+
+	protected function validateSubdivisions($values, AddressFormatInterface $addressFormat, $constraint)
+    {
+        $dataProvider = $this->getDataProvider();
+        $countryCode = $addressFormat->getCountryCode();
+        $subdivisionLevels = array(
+            'root',
+            AddressFormatInterface::FIELD_ADMINISTRATIVE_AREA,
+            AddressFormatInterface::FIELD_LOCALITY,
+            AddressFormatInterface::FIELD_DEPENDENT_LOCALITY,
+        );
+        $subdivisions = array();
+        foreach ($subdivisionLevels as $index => $fieldConstant) {
+            $parentId = ($fieldConstant == 'root') ? 0 : $values[$fieldConstant];
+            $children = $dataProvider->getSubdivisions($countryCode, $parentId);
+            $nextIndex = $index + 1;
+            if (!$children || !isset($subdivisionLevels[$nextIndex])) {
+                // This level has no children, stop.
+                break;
+            }
+            $nextFieldConstant = $subdivisionLevels[$nextIndex];
+            if (empty($values[$nextFieldConstant])) {
+                // The child value is empty, stop.
+                break;
+            }
+
+            $found = false;
+            foreach ($children as $child) {
+                if ($child->getId() == $values[$nextFieldConstant]) {
+                    $found = true;
+                    $subdivisions[] = $child;
+                    break;
+                }
+            }
+
+            if (!$found) {
+                $subPath = '[' . $this->fieldMapping[$nextFieldConstant] . ']';
+                $invalidValue = $values[$nextFieldConstant];
+                $this->context->addViolationAt($subPath, $constraint->invalidMessage, array(), $invalidValue);
+                break;
+            }
+        }
+
+        return $subdivisions;
+    }
+
+
 	public static function init() {
 		extract(Message::aliases());
 		$model = static::_object();
 
-		$model->validates['name'] = [
+
+		static::applyFilter('validate', function() {
+			$sfValidator = SymfonyValidation::createValidator();
+
+			$violations = $sfValidator->validateValue($entity->formal()->getContryCode(), new Country());
+			$violations = $sfValidator->validateValue($entity->formal(), new AddressFormat());
+
+
+			return $chain;
+		});
+
+		// The following rules only check for existencce of fields.
+		// The rest of the work is left to the addressing validator.
+
+		$model->validates['recipient'] = [
 			[
 				'nameOrCompany',
 				'on' => ['create', 'update'],
 				'message' => 'Bitte geben Sie einen Namen und/oder eine Firma an.'
 			]
 		];
-		Validator::add('nameOrCompany', function($value, $format, $options) {
-			return !empty($value) || !empty($options['values']['company']);
+		Validator::add('recipientOrOrganization', function($value, $format, $options) {
+			return !empty($value) || !empty($options['values']['organization']);
 		});
-		$model->validates['street'] = [
+		$model->validates['address_line_1'] = [
 			[
 				'notEmpty',
 				'on' => ['create', 'update'],
@@ -72,34 +180,29 @@ class Addresses extends \base_core\models\Base {
 		if (strpos($locale, '_') === false) {
 			$locale = $locale . '_' . strtoupper($locale);
 		}
-		$model->validates['zip'] = [
+		$model->validates['postal_code'] = [
 			[
 				'notEmpty',
 				'on' => ['create', 'update'],
 				'last' => true,
 				'message' => $t('This field cannot be empty.')
-			],
-			[
-				'postalCode',
-				'on' => ['create', 'update'],
-				'format' => $locale,
-				'message' => $t('The field is not correctly formatted.')
-			],
+			]
 		];
-		$model->validates['city'] = [
+		$model->validates['locality'] = [
 			[
 				'notEmpty',
 				'on' => ['create', 'update'],
 				'message' => $t('This field cannot be empty.')
 			],
 		];
-		$model->validates['country'] = [
+		$model->validates['country_code'] = [
 			[
 				'notEmpty',
 				'on' => ['create', 'update'],
 				'message' => $t('A country must be selected.')
 			],
 		];
+
 		$model->validates['phone'] = [
 			[
 				'phone',
@@ -151,41 +254,43 @@ class Addresses extends \base_core\models\Base {
 		return static::create($item);
 	}
 
-	public function format($entity, $type, $locale = null) {
-		if (!$locale) {
-			$locale = ($user = $entity->user()) ? $user->locale : Environment::get('locale');
+	public function format($entity, $type, $originCountryCode, $originLocale = null) {
+		if (!$originLocale) {
+			$originLocale = ($user = $entity->user()) ? $user->locale : Environment::get('locale');
 		}
+		$formatter = new PostalFormatter(new DataProvider());
 
 		if ($type == 'oneline') {
 			$result = [];
 
-			$result[] = $entity->company;
-			$result[] = $entity->name;
-			$result[] = $entity->street;
-			$result[] = $entity->city;
+			$result[] = $entity->organization;
+			$result[] = $entity->recipient;
+			$result[] = $entity->address_line_1;
+			$result[] = $entity->locality;
 
 			return implode(', ', array_filter($result));
 		}
 		if ($type == 'postal') {
-			$result = [];
-
-			$result[] = $entity->company;
-
-			if ($entity->company) {
-				$result[] = "– {$entity->name} –";
-			} else {
-				$result[] = $entity->name;
-			}
-			$result[] = $entity->street;
-			$result[] = $entity->zip . ' ' . $entity->city;
-			$result[] = Countries::find('first', [
-				'conditions' => [
-					'id' => $entity->country
-				],
-				'locale' => $locale
-			])->name;
-			return implode("\n", array_filter($result));
+			return $formatter->format($entity->formal($originLocale), $originCountryCode, $originLocale);
 		}
+	}
+
+	public function formal($entity, $locale = null) {
+		if (!$locale) {
+			$locale = Environment::get('locale');
+		}
+		return (new FormalAddress())
+			->setLocale($locale)
+			->setCountryCode($entity->country_code)
+			->setAdministrativeAreay($entity->administrative_area)
+			->setLocality($entity->locality)
+			->setDependentLocality($entity->dependent_locality)
+			->setPostalCode($entity->postal_code)
+			->setSortingCode($entity->sorting_code)
+			->setAddressLine1($entity->address_line_1)
+			->setAddressLine2($entity->address_line_2)
+			->setOrganization($entity->organization)
+			->setRecipient($entity->recipient);
 	}
 
 	// FIXME Auto map regions to prefixes.

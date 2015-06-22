8a99ccd921dac734977bc4c21b51de7cbe967149
David Persson <davidpersson@gmx.de>
2015-06-22 13:58:09 +0200

    Update schema.

--- data/schema.sql
+++ data/schema.sql
@@ -2,12 +2,16 @@ CREATE TABLE `addresses` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(11) unsigned DEFAULT NULL,
   `virtual_user_id` int(11) unsigned DEFAULT NULL,
-  `name` varchar(250) DEFAULT '',
-  `company` varchar(250) DEFAULT NULL,
-  `street` varchar(250) DEFAULT NULL,
-  `city` varchar(100) DEFAULT NULL,
-  `zip` varchar(100) DEFAULT NULL,
+  `recipient` varchar(250) DEFAULT '',
+  `organization` varchar(250) DEFAULT NULL,
+  `address_line_1` varchar(250) DEFAULT NULL,
+  `address_line_2` varchar(250) DEFAULT NULL,
+  `locality` varchar(100) DEFAULT NULL COMMENT 'city',
+  `dependent_locality` varchar(100) DEFAULT NULL COMMENT 'neighborhood',
+  `postal_code` varchar(100) DEFAULT NULL COMMENT 'zip',
+  `sorting_code` varchar(100) DEFAULT NULL COMMENT 'CEDEX',
   `country` char(2) DEFAULT 'DE',
+  `administrative_area` varchar(200) DEFAULT NULL,
   `phone` varchar(200) DEFAULT NULL,
   `created` datetime NOT NULL,
   `modified` datetime DEFAULT NULL,
@@ -15,4 +19,3 @@ CREATE TABLE `addresses` (
   KEY `user_id` (`user_id`),
   KEY `virtual_user_id` (`virtual_user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-

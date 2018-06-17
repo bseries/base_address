a9509027b1cd7125cd0ca4bcf69e41ee67339651
Marius Wilms <mariuswilms@mailbox.org>
2018-06-17 19:37:22 +0200

    Inherit default charset and collation

--- data/upgrade/001-g11n.sql
+++ data/upgrade/001-g11n.sql
@@ -1,12 +1,12 @@
 ALTER TABLE `addresses` ADD `administrative_area` VARCHAR(200)  NULL  DEFAULT NULL  AFTER `zip`;
-ALTER TABLE `addresses` CHANGE `city` `locality` VARCHAR(100)  CHARACTER SET utf8  COLLATE utf8_general_ci  NULL  DEFAULT NULL  COMMENT 'city';
+ALTER TABLE `addresses` CHANGE `city` `locality` VARCHAR(100)  NULL  DEFAULT NULL  COMMENT 'city';
 ALTER TABLE `addresses` ADD `dependent_locality` VARCHAR(100)  NULL  DEFAULT NULL  COMMENT 'neighborhood'  AFTER `locality`;
-ALTER TABLE `addresses` CHANGE `zip` `postal_code` VARCHAR(100)  CHARACTER SET utf8  COLLATE utf8_general_ci  NULL  DEFAULT NULL  COMMENT 'zip';
-ALTER TABLE `addresses` CHANGE `street` `address_line_1` VARCHAR(250)  CHARACTER SET utf8  COLLATE utf8_general_ci  NULL  DEFAULT NULL;
+ALTER TABLE `addresses` CHANGE `zip` `postal_code` VARCHAR(100)  NULL  DEFAULT NULL  COMMENT 'zip';
+ALTER TABLE `addresses` CHANGE `street` `address_line_1` VARCHAR(250)  NULL  DEFAULT NULL;
 ALTER TABLE `addresses` ADD `address_line_2` VARCHAR(250)  NULL  DEFAULT NULL  AFTER `address_line_1`;
 ALTER TABLE `addresses` ADD `sorting_code` VARCHAR(100)  NULL  DEFAULT NULL  COMMENT 'CEDEX'  AFTER `postal_code`;
-ALTER TABLE `addresses` CHANGE `company` `organization` VARCHAR(250)  CHARACTER SET utf8  COLLATE utf8_general_ci  NULL  DEFAULT NULL;
-ALTER TABLE `addresses` MODIFY COLUMN `administrative_area` VARCHAR(200) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL AFTER `country`;
-ALTER TABLE `addresses` CHANGE `name` `recipient` VARCHAR(250)  CHARACTER SET utf8  COLLATE utf8_general_ci  NULL  DEFAULT '';
+ALTER TABLE `addresses` CHANGE `company` `organization` VARCHAR(250)  NULL  DEFAULT NULL;
+ALTER TABLE `addresses` MODIFY COLUMN `administrative_area` VARCHAR(200) DEFAULT NULL AFTER `country`;
+ALTER TABLE `addresses` CHANGE `name` `recipient` VARCHAR(250)  NULL  DEFAULT '';
 
 
